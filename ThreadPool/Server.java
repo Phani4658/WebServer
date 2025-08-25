2bbe1db0c5ad749d251760ac438837b156704ecb
Phani <mysteriousphani@gmail.com>
2025-08-26 01:48:01 +0530

    Implement Server with ThreadPool

---
+++ ThreadPool/Server.java
@@ -0,0 +1,44 @@
+package ThreadPool;
+
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    private final ExecutorService threadPool;
+    
+    public Server(int poolSize){
+        this.threadPool = Executors.newFixedThreadPool(poolSize);
+    }
+
+    public void handleClient(Socket clientSocket) {
+        try (PrintWriter toSocket = new PrintWriter(clientSocket.getOutputStream(), true)) {
+            toSocket.println("Hello From the Server: " +  clientSocket.getInetAddress());
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int port = 8080;
+        int poolSize = 10;
+        Server server = new Server(poolSize);
+        
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            serverSocket.setSoTimeout(10000);
+            System.out.println("Server is listening on port " + port);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                
+                server.threadPool.execute(() -> server.handleClient(clientSocket));
+            }
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
